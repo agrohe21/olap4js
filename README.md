b20b57f533b920ed2467b033c59c9ccf5c9b86a6
Andy Grohe <agrohe21@gmail.com>
2012-05-31 15:13:39 -0500

    added node test

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Alpha code at this point.
+
+# Overview
+olap4js is intended to be a common set of JS objects that can run on multiple clients and with multiple OLAP backends
+
+# Host Environments
+## Browser: Main testing evnironment at the moment
+Xmla The XMLA implementation depends on [Xmla4js](http://code.google.com/p/xmla4js/).
+## Node.js
+the basic olap connection is working, but not Xmla
---
+++ test/test-olap-node.js
@@ -0,0 +1,25 @@
+var olap = require('../olap'),
+assert = require('assert');
+
+
+var sources, idx, catalogs, catalog, cuubes, cube, dimensions, dimension, hierarchies, hierarchy, levels, level, members, member, measures, measure;
+var mysrc = {
+    AUTHENTICATION_MODE:"Unauthenticated",
+    DATA_SOURCE_DESCRIPTION: "Pentaho BI Platform Datasources",
+    DATA_SOURCE_INFO:"Provider=Mondrian;DataSource=Pentaho",
+    DATA_SOURCE_NAME:"Provider=Mondrian;DataSource=Pentaho",
+    PROVIDER_NAME:"PentahoXMLA",
+    PROVIDER_TYPE:["MDP"],
+    URL: "http://localhost:8080/pentaho/Xmla?userid=joe&password=password",
+    catalogs: [
+	{
+	CATALOG_NAME: "SampleData",	
+	DESCRIPTION:"No description available"
+	}			
+    ]
+}, olapConn;
+
+olapConn = new olap.Connection({sources:[mysrc]});
+sources = olapConn.getOlapDatabases();
+
+assert.equal(sources[0].PROVIDER_NAME, "PentahoXMLA")
